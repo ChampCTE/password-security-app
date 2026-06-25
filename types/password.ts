56dd3511fdb732a44660eb983a51f53905e3ca26
ChampCTE <celia.trulla@gmail.com>
2026-06-25 10:58:11 +0200

    LLamada del generador a API

---
+++ types/password.ts
@@ -0,0 +1,14 @@
+// types/password.ts
+// Type definitions for password generator and analyzer
+
+export type GeneratePasswordRequest = {
+  length: number;
+  useUpper: boolean;
+  useLower: boolean;
+  useNumbers: boolean;
+  useSymbols: boolean;
+};
+
+export type GeneratePasswordResponse = {
+  password: string;
+};
